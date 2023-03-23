c13d68963c6ac20ae6f913eb430f6b4be4bcf54d
Roberto2194 <roberto_liccardo@outlook.it>
2023-03-23 18:56:25 +0100

    removed the Board type and related changes; more areValuesValid additions/modifications

--- src/Main.java
+++ src/Main.java
@@ -16,8 +16,8 @@ public class Main {
 
         do {
             System.out.println("Please enter a valid board configuration");
-            System.out.println("The total number of tiles must be even");
-            System.out.println("The board must have at lease 4 tiles\n");
+            System.out.println("Remember that the total number of tiles must be even");
+            System.out.println("And the board must have at lease 4 tiles\n");
 
             System.out.print("Number of rows: ");
             rows = input.nextInt();
@@ -26,28 +26,42 @@ public class Main {
             columns = input.nextInt();
         } while (!isConfigurationValid(rows, columns));
 
+        // we can see the board as a simple matrix of rows and cols
+        int[][] board = new int[rows][columns];
+
         System.out.print('\n');
 
         char[] symbols = makeSymbols(rows, columns);
         Card[] cards = createCards(symbols);
+
         shuffleCards(cards);
 
-        Board board = new Board(rows, columns, cards);
-        board.drawBoard();
+        // TODO: - we have to populate the "int[][] board" matrix with cards at every position.
+        drawBoard(rows, columns, cards);
 
         System.out.print('\n');
 
-        System.out.print("First card - row: ");
-        int firstCardRow = input.nextInt();
-        System.out.print("First card - column: ");
-        int firstCardColumn = input.nextInt();
+        int firstCardRow;
+        int firstCardColumn;
+        int secondCardRow;
+        int secondCardColumn;
 
-        System.out.print("Second card - row: ");
-        int secondCardRow = input.nextInt();
-        System.out.print("Second card - column: ");
-        int secondCardColumn = input.nextInt();
+        do {
+            System.out.println("Please enter valid card values");
+            System.out.println("First and second card must be different");
+            System.out.println("Cards must be inside the boundaries of the board");
+            System.out.println("Cards must NOT correspond to already reveled cards\n");
 
+            System.out.print("First card - row: ");
+            firstCardRow = input.nextInt();
+            System.out.print("First card - column: ");
+            firstCardColumn = input.nextInt();
 
+            System.out.print("Second card - row: ");
+            secondCardRow = input.nextInt();
+            System.out.print("Second card - column: ");
+            secondCardColumn = input.nextInt();
+        } while (!areValuesValid(rows, columns, firstCardRow, firstCardColumn, secondCardRow, secondCardColumn));
     }
 
     /**
@@ -112,8 +126,26 @@ public class Main {
     }
 
     /**
+     * Draws the board using the number of rows and columns stored
+     *
+     * @param rows    the number of rows in the board
+     * @param columns the number of columns in the board
+     * @param cards   the cards to be displayed inside every tile
+     */
+    public static void drawBoard(int rows, int columns, Card[] cards) {
+        int cardNum = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(cards[cardNum].faceDown());
+                cardNum++;
+            }
+            System.out.print("\n");
+        }
+    }
+
+    /**
      * Checks whether the configuration that we get as an input
-     * from the user is valid, otherwise we can not create the board.
+     * from the user is valid, otherwise we can not create the board.<br>
      * For the configuration to be valid we need at lest 4 tiles,
      * and if so, the total number of tiles must be even.
      *
@@ -129,16 +161,35 @@ public class Main {
 
     /**
      * Checks whether the values that the user inserted for
-     * rows and column are valid. That is:
-     * 1 - whether they are inside the boundaries of the board
-     * 2 - whether they correspond to an already reveled card
+     * rows and column are valid. That is:<br>
+     * 1- first and second card must be different<br>
+     * 2- cards must be inside the boundaries of the board;<br>
+     * 3- cards must NOT correspond to already reveled cards.
      *
-     * @param row    the row of the card
-     * @param column the column of the card
+     * @param boardRows        the number of rows that the board has
+     * @param boardColumns     the number of columns that the board has
+     * @param firstCardRow     the row of the first card
+     * @param firstCardColumn  the column of the first card
+     * @param secondCardRow    the row of the second card
+     * @param secondCardColumn the column of the second card
      * @return true or false based on whether the inserted one is a valid position or not
      */
-    public static boolean areValuesValid(int row, int column) {
-
+    public static boolean areValuesValid(
+            int boardRows,
+            int boardColumns,
+            int firstCardRow,
+            int firstCardColumn,
+            int secondCardRow,
+            int secondCardColumn
+    ) {
+        // 1- first and second card must be different
+        if (firstCardRow == secondCardRow && firstCardColumn == secondCardColumn) return false;
+        // 2- are we inside the boundaries of the board
+        if (firstCardRow > boardRows || firstCardColumn > boardColumns) return false;
+        // 3- are the col and row inserted the ones of an already revealed card
+        // TODO : - implement point number 3)
+        return false;
     }
+
 
 }
